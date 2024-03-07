7168b5092fd323f8fa63a1b42fb71c70acba37af
Adam Carlin <1093212+blissend@users.noreply.github.com>
2024-03-07 17:07:38 -0500

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -12,10 +12,10 @@ const char* ssid = "asdf";
 const char* password = "asdf";
 
 // I2S configuration
+#define PIN_CLK     0
+#define PIN_DATA    34
 const i2s_port_t I2S_PORT = I2S_NUM_0;
 const int SAMPLE_RATE = 44100;
-const int SAMPLE_BITS = 16;
-const int CHANNEL_NUM = 1;
 const int SAMPLE_BUFFER_SIZE = 1024;
 
 int16_t sampleBuffer[SAMPLE_BUFFER_SIZE];
@@ -25,8 +25,12 @@ void setupI2S() {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
     .sample_rate = SAMPLE_RATE,
     .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_MONO,
-    .communication_format = I2S_COMM_FORMAT_I2S,
+    .channel_format = I2S_CHANNEL_FMT_ALL_RIGHT,
+#if ESP_IDF_VERSION > ESP_IDF_VERSION_VAL(4, 1, 0)
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+#else
+        .communication_format = I2S_COMM_FORMAT_I2S,
+#endif
     .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 2,
     .dma_buf_len = 128,
@@ -34,14 +38,18 @@ void setupI2S() {
 
   i2s_pin_config_t pin_config = {
     .bck_io_num = I2S_PIN_NO_CHANGE,
-    .ws_io_num = GPIO_NUM_0,
+    .ws_io_num = PIN_CLK,
     .data_out_num = I2S_PIN_NO_CHANGE,
-    .data_in_num = GPIO_NUM_34
+    .data_in_num = PIN_DATA
   };
+
+#if (ESP_IDF_VERSION > ESP_IDF_VERSION_VAL(4, 3, 0))
+    pin_config.mck_io_num = I2S_PIN_NO_CHANGE;
+#endif
 
   i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
   i2s_set_pin(I2S_PORT, &pin_config);
-  i2s_set_clk(I2S_PORT, SAMPLE_RATE, SAMPLE_BITS, I2S_CHANNEL_MONO);
+  i2s_set_clk(I2S_PORT, SAMPLE_RATE, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
 }
 
 void sendToPrometheus(float decibelLevel) {
